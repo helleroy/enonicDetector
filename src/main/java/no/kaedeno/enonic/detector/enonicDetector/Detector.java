addb25e76ad3a160caee6fadcf4c91d771e3c444
Henrik Hellerøy <helleroy@gmail.com>
2012-11-28 14:08:29 +0100

    Added methods for sending client tests

--- src/main/java/no/kaedeno/enonic/detector/enonicDetector/Detector.java
+++ src/main/java/no/kaedeno/enonic/detector/enonicDetector/Detector.java
@@ -1,5 +1,7 @@
 package no.kaedeno.enonic.detector.enonicDetector;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.logging.Logger;
 
 import javax.servlet.http.HttpServletRequest;
@@ -11,24 +13,19 @@ import ua_parser.Parser;
 import ua_parser.Client;
 
 public class Detector extends HttpInterceptor {
-	
-	@Override
-	public void postHandle(HttpServletRequest httpServletRequest,
-			HttpServletResponse httpServletResponse) throws Exception {
-		// TODO Auto-generated method stub
-	}
 
 	@Override
 	public boolean preHandle(HttpServletRequest httpServletRequest,
 			HttpServletResponse httpServletResponse) throws Exception {
 
-		/*
-		 * 1. Look up UA string in cache/database 2. If found: 2.1. Set UA
-		 * features in context XML 3. If not found: 3.1. Send modernizr tests to
-		 * client 3.2. Check UA string for useful information 3.3. Store
-		 * modernizr and UA string result in cache/database 3.4. Set UA features
-		 * in context XML
-		 */
+		// 1. Look up UA string in cache/database
+		// 2. If found:
+		// 2.1. Set UA features in context XML
+		// 3. If not found:
+		// 3.1. Send modernizr tests to client
+		// 3.2. Check UA string for useful information
+		// 3.3. Store modernizr and UA string result in cache/database
+		// 3.4. Set UA features in context XML
 
 		String userAgent = httpServletRequest.getHeader("User-Agent");
 		Logger log = Logger.getLogger("Detector");
@@ -48,8 +45,30 @@ public class Detector extends HttpInterceptor {
 		log.info("Device Family: " + c.device.family);
 		log.info("Device is mobile: " + new Boolean(c.device.isMobile).toString());
 		log.info("Device is a spider: " + new Boolean(c.device.isSpider).toString());
-		
+
 		return true;
 	}
 
+	@Override
+	public void postHandle(HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse) throws Exception {
+		// Do nothing
+	}
+
+	private String generateMarkup() {
+		// TODO: Generate markup
+		return null;
+	}
+
+	private boolean sendClientTests(HttpServletResponse httpServletResponse) {
+		String markup = this.generateMarkup();
+		try {
+			PrintWriter w = httpServletResponse.getWriter();
+			w.write(markup);
+			return true;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
 }
