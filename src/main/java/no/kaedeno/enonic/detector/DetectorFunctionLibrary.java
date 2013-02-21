c09d62a3685d8c02bead47a61479bd2aa479e04b
Henrik Hellerøy <helleroy@gmail.com>
2013-02-21 01:48:47 +0100

    Added FunctionLibrary function for getting the UA feature data as XML. Started work on the FunctionLibrary function that gets the UA family.

--- src/main/java/no/kaedeno/enonic/detector/DetectorFunctionLibrary.java
+++ src/main/java/no/kaedeno/enonic/detector/DetectorFunctionLibrary.java
@@ -1,20 +1,27 @@
 package no.kaedeno.enonic.detector;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.StringReader;
-import java.net.UnknownHostException;
+import java.io.StringWriter;
+import java.util.Scanner;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 
 import org.jdom.Document;
 import org.jdom.JDOMException;
 import org.jdom.input.SAXBuilder;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.json.XML;
 import org.xml.sax.InputSource;
 
 import com.enonic.cms.api.plugin.PluginConfig;
 import com.enonic.cms.api.plugin.PluginEnvironment;
-import com.mongodb.DBObject;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class DetectorFunctionLibrary {
 
@@ -24,43 +31,71 @@ public class DetectorFunctionLibrary {
 	private DetectorDAO<UserAgent> dao = null;
 
 	/**
-	 * Gets all detected features of the requesting user agent from the database 
+	 * Gets all detected features of the requesting user agent from the database
 	 * as an XML document.
 	 * 
 	 * @return a JDOM XML Document
 	 */
 	public Document getUAFeaturesXML() {
-		
+
 		String userAgent = pluginEnvironment.getCurrentRequest().getHeader("User-Agent");
 		UserAgent result = dao.findOne("userAgent", userAgent);
-		
-		// Build the JDOM XML Document object from the database result
-//		try {
-//			return new SAXBuilder().build(new InputSource(new StringReader(mongoToXML(result))));
-//		} catch (JDOMException e) {
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-		return null;
-	}
-	
-	/**
-	 * Converts a MongoDB DBObject to an XML-formatted String
-	 * 
-	 * @param mongo the MongoDB DBObject 
-	 * @return an XML-formatted String
-	 */
-	private String mongoToXML(DBObject mongo) {
+
 		try {
-			return XML.toString(new JSONObject(mongo.toString()));
-		} catch (JSONException e) {
+			// Marshal the UserAgent result to XML
+			JAXBContext context = JAXBContext.newInstance(UserAgent.class);
+			Marshaller marshaller = context.createMarshaller();
+			StringWriter stringWriter = new StringWriter();
+			marshaller.marshal(result, stringWriter);
+
+			// Build the JDOM XML Document object from the marshalled result
+			return new SAXBuilder()
+					.build(new InputSource(new StringReader(stringWriter.toString())));
+
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		} catch (JDOMException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		return null;
 	}
 
-	
+	/**
+	 * Gets the user agent family of the requesting user agent from as a string.
+	 * The family is decided by the user-defined family definition JSON
+	 * 
+	 * @return the user agent family as a string
+	 */
+	public String getUAFamily() {
+		String familiesJSONFile = (String) pluginConfig.get("families.uri");
+		String familiesJSON;
+
+		File file = new File(familiesJSONFile);
+		Scanner sc;
+		try {
+			sc = new Scanner(file);
+		} catch (FileNotFoundException e) {
+			InputStream is = getClass().getClassLoader()
+					.getResourceAsStream("/" + familiesJSONFile);
+			sc = new Scanner(is);
+		}
+		familiesJSON = sc.useDelimiter("\\Z").next();
+		sc.close();
+
+		try {
+			ObjectMapper mapper = new ObjectMapper();
+			JsonNode jsonObject = mapper.readTree(familiesJSON);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
 	public void setPluginConfig(PluginConfig pluginConfig) {
 		this.pluginConfig = pluginConfig;
 	}
@@ -68,7 +103,7 @@ public class DetectorFunctionLibrary {
 	public void setPluginEnvironment(PluginEnvironment pluginEnvironment) {
 		this.pluginEnvironment = pluginEnvironment;
 	}
-	
+
 	public void setDao(DetectorDAO<UserAgent> dao) {
 		this.dao = dao;
 	}
